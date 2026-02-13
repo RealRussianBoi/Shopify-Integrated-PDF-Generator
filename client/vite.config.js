b801bf8e57c4fe59ddfae59072ab6cd3dc0cba56
RealRussianBoi <dekingdomcrafter@gmail.com>
2026-02-12 19:33:25 -0500

    Deployed client to github pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Shopify-Integrated-PDF-Generator/",
   plugins: [react()],
-})
+});
+
